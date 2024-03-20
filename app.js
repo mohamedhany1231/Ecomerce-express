4be6a36ec1ef8a5162663ee7277b5f15bd5866f0
mohamedhany1231 <mohamedhany61@gmail.com>
2024-03-20 08:19:43 +0200

    server static files fix

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
