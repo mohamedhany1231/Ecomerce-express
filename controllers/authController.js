d24114c109aaae7b74bc4a1dad31e874a09278ca
mohamedhany1231 <mohamedhany61@gmail.com>
2024-08-13 10:03:12 +0300

    fix domain

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,18 +13,14 @@ const signToken = (id) =>
 const sendSignToken = (res, user, statusCode, req) => {
   const token = signToken(user.id);
 
-  const origin = req.get("origin") || "http://localhost:5173";
-
-  const domain = new URL(origin).hostname;
-  console.log(domain);
-
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     sameSite: "none",
-    domain,
+    domain:
+      process.env.NODE_ENV !== "development" && "ecomerce-3qnz.onrender.com",
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
   res.cookie("jwt", token, cookieOptions);
